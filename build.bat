78ee4916e10e8bd3a9e749ca5e08f040c3481582
MichaelSharich <michael.sharich@gmail.com>
2025-04-15 16:52:20 -0600

    Update build.bat for RestSharp 112
    Update build.bat for RestSharp 112

--- build.bat
+++ build.bat
@@ -3,15 +3,21 @@
 
 @echo off
 
-SET CSCPATH=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319
 
 if not exist ".\nuget.exe" powershell -Command "(new-object System.Net.WebClient).DownloadFile('https://dist.nuget.org/win-x86-commandline/latest/nuget.exe', '.\nuget.exe')"
 .\nuget.exe install src\IO.ClickSend\packages.config -o packages
 
 if not exist ".\bin" mkdir bin
 
+dotnet --version >nul 2>&1
+IF ERRORLEVEL 1 (
+    echo .NET SDK not found. Please install it from https://dotnet.microsoft.com/download
+    pause
+    exit /b
+)
+
 copy packages\Newtonsoft.Json.13.0.3\lib\netstandard2.0\Newtonsoft.Json.dll bin\Newtonsoft.Json.dll
 copy packages\RestSharp.112.1.0\lib\net48\RestSharp.dll bin\RestSharp.dll
 copy packages\JsonSubTypes.2.0.1\lib\netstandard2.0\JsonSubTypes.dll bin\JsonSubTypes.dll
-%CSCPATH%\csc  /reference:bin\Newtonsoft.Json.dll;bin\JsonSubTypes.dll;bin\RestSharp.dll /target:library /out:bin\IO.ClickSend.dll /recurse:src\IO.ClickSend\*.cs /doc:bin\IO.ClickSend.xml
 
+dotnet build src/IO.ClickSend/IO.ClickSend.csproj --configuration Debug -o bin\IO.ClickSend.dll --source packages
